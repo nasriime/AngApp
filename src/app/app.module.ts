6b36c907d115bc3644d8cea7ebf9b77ecefd32a5
Mohamed NasrAllah <m.nasrallah@indielabs.sa>
2018-06-07 15:51:33 +0200

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { SelectRoleComponent } from './select-role/select-role.component';
+
+
+
+const appRoutes: Routes = [
+    { path: '', component: AuthComponent },
+    { path: 'selectrole', component: SelectRoleComponent },
+    { path: '**', component: AppComponent }
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AuthComponent,
+        SelectRoleComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(
+            appRoutes,
+            { enableTracing: true } // <-- debugging purposes only
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/select-role/select-role.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-role',
+  templateUrl: './select-role.component.html',
+  styleUrls: ['./select-role.component.scss']
+})
+export class SelectRoleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
